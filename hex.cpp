925e8b07a4e8160e3e642044e7fee86f8c272d56
Maxim Masiutin <maxim@masiutin.com>
2026-01-08 20:06:32 +0200

    Use explicit if-guards for unsigned subtraction to satisfy Snyk static analysis

--- hex.cpp
+++ hex.cpp
@@ -1956,10 +1956,12 @@ int main() {
         if (row < first_row_base)
           continue;
 
-        // column >= first_column_base and row >= first_row_base are guaranteed
-        // by the continue statements above
-        unsigned int ux = column - first_column_base;
-        unsigned int uy = row - first_row_base;
+        unsigned int ux = 0;
+        if (column >= first_column_base)
+          ux = column - first_column_base;
+        unsigned int uy = 0;
+        if (row >= first_row_base)
+          uy = row - first_row_base;
         if (field.coord_out_of_range_xy(ux, uy)) {
           std::cout << "Invalid coordinates entered (" << row << "," << column
                     << ") Row should be between 1 and " << field.width
